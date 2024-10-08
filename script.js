1e2386e0ae84b6c7c4c67f9637aa2c095772df57
ryan woods <federer5050@gmail.com>
2024-10-08 19:51:08 +0100

    slower horizontal scroll

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-let carousel = document.querySelector(".carousel");
-let slides = document.querySelectorAll(".slide");
-let carouselWidth;
-let slideWidth;
+let items = document.querySelectorAll(".item");
+let slider = document.querySelector(".slider");
+let sliderWidth;
+let itemWidth;
 let currentPos = 0;
 
 function init() {
-  carouselWidth = carousel.getBoundingClientRect().width;
-  slideWidth = carouselWidth / slides.length;
+  sliderWidth = slider.getBoundingClientRect().width;
+  itemWidth = sliderWidth / items.length;
   document.body.style.height = `${
-    carouselWidth - (window.innerWidth - window.innerHeight)
+    sliderWidth - (window.innerWidth - window.innerHeight)
   }px`;
 }
 
-function setCarouselWidth() {
+function setSliderWidth() {
   let totalWidth = 0;
-  slides.forEach((slide) => {
-    totalWidth += slide.offsetWidth;
+  items.forEach((item) => {
+    totalWidth += item.offsetWidth;
   });
 
-  carousel.style.width = `${totalWidth}px`;
+  slider.style.width = `${totalWidth}px`;
 }
 
 function animate() {
-  init();
-  setCarouselWidth();
-  currentPos = window.scrollY;
-  carousel.style.transform = `translateX(${-currentPos}px)`;
-  requestAnimationFrame(animate);
+  setTimeout(() => {
+    init();
+    setSliderWidth();
+    currentPos = window.scrollY;
+    slider.style.transform = `translateX(${-currentPos}px)`;
+    requestAnimationFrame(animate);
+  }, 150); // Delay of half a second (500 milliseconds)
 }
+
+animate(); // Start the animation loop
 
 animate();
